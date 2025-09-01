b88d58ff050861c587f34e5e95124a22fb66e4a8
dhruvsahu007 <114899200+dhruvsahu007@users.noreply.github.com>
2025-09-01 18:28:34 +0530

    Enhance User and Transactions models by adding primary key and unique constraints

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer)
-    email = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
     password = Column(String)
     phone_number = Column(Integer)
     balance = Column(Integer, default=0)
@@ -15,7 +15,7 @@ class User(Base):
 class Transactions(Base):
     __tablename__ = "transactions"
 
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
     transaction_type = Column(String)
     amount = Column(Integer)
